5ce24371e6e1ae82c124baebf771603e3894ef4d
rohrohh13 <r.vidon@datack.fr>
2025-04-11 15:22:12 +0200

    Ajout de nouvelles fonctionnalités

--- config.js
+++ config.js
@@ -4,10 +4,3 @@ export const config = {
   medalsUrl: `${API_BASE_URL}/api/medailles`,
   athletesUrl: `${API_BASE_URL}/api/athletes`,
 };
-
-
-
-/*export const config = {
-  medalsUrl: 'https://opensheet.elk.sh/1DvKrqg4A1wTXDOi1_l_w5PlHuzxGKKm-MYHoWZjUuTk/medaille',
-  athletesUrl: 'https://opensheet.elk.sh/1DvKrqg4A1wTXDOi1_l_w5PlHuzxGKKm-MYHoWZjUuTk/sportif',
-};*/
